34d6c5a813450af241535900692c94f558ceb3d8
Girish Pandit <girishvpandit@gmail.com>
2021-05-08 21:33:19 +0530

    Sunshine To-Do list app. First Commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Sunshine To-do list
+        </q-toolbar-title>
+
+        
+      </q-toolbar>
+    </q-header>
+
+ 
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'MainLayout',
+ 
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-page padding>
+    <div class="row" q-mb-lg style="max-width: 450px">
+        <q-input outlined v-model="newTask" placeholder="Add new task" class="col" @keyup.enter="addTask"/>
+      <q-btn push color="primary" text-color="white" label="Add" @click.native="addTask" />
+    </div>
+
+
+     <div class="q-pa-lg" q-mb-lg style="max-width: 450px">
+       <q-list-header> Tasks </q-list-header>
+            <q-list bordered v-for="(task, index) in tasks" :key="index">
+              <q-item >
+                <q-item-section>{{task}}</q-item-section>
+                <q-item-section avatar>
+                  <q-icon color="primary" name="eva-checkmark-outline" @click.native="moveToDone(index)"/>
+                </q-item-section>
+              </q-item>
+            </q-list>
+     </div>
+
+      <div class="q-pa-lg" style="max-width: 450px">
+       <q-list-header> Done </q-list-header>
+            <q-list bordered v-for="(task, index) in done" :key="index">
+              <q-item >
+                <q-item-section>{{task}}</q-item-section>
+                <q-item-section avatar>
+                  <q-icon color="red" name="eva-close-outline" @click.native="deleteTask(index)" />
+                </q-item-section>
+              </q-item>
+            </q-list>
+     </div>
+
+  </q-page> 
+</template>
+
+<script>
+          export default {
+            name: 'PageIndex',
+            data () {
+              return{
+                tasks:[],
+                done:[],
+                newTask:''
+              }
+            },
+            methods:{
+              addTask(){
+                this.tasks.push(this.newTask)
+                this.newTask=''
+              },
+              moveToDone(index){
+                this.done.push(this.tasks[index]),
+                this.tasks.splice(index, 1)
+              },
+              deleteTask(index){
+              this.done.splice(index, 1)
+            },
+            }  
+          }   
+</script>
